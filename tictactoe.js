0e8f5530c121ca05d344bead331ad12ff5edd56e
eeeeoino <eetu.e.oinonen@student.jyu.fi>
2025-04-05 12:41:15 +0300

    Aloitettiin projekti alusta, nyt se on jopa lähes toimiva

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,10 @@
 const start = document.getElementById("start");
 start.addEventListener("click", GameController);
 
+const again = document.getElementById("again");
+again.style.display = 'none';
+again.addEventListener("click", newGame);
+
 const p1 = document.getElementById("player1");
 const p2 = document.getElementById("player2");
 const turn = document.getElementById("turn");
@@ -8,8 +12,27 @@ const result = document.getElementById("result");
 const cells = document.querySelectorAll(".cell");
 
 const empty = "";
+let gameOver = false;
+let isFirstTime = true;
 
-function GameBoard() {
+const playerOneName = prompt("Give the name of player1: ");
+    const playerTwoName = prompt("Give the name of player2: ");
+    const playerOneMarker = prompt("Give the marker of player1: ");
+    const playerTwoMarker = prompt("Give the marker of player2: ");
+
+    const players = [
+        {
+            name: playerOneName,
+            marker: playerOneMarker
+        },
+        {
+            name: playerTwoName,
+            marker: playerTwoMarker 
+        }
+    ]; 
+
+
+const game = (function GameBoard() {
     const rows = 3;
     const columns = 3;
     const board = [];
@@ -24,36 +47,23 @@ function GameBoard() {
     const getBoard = () => board;
 
     const placeMarker = (cell, player) => {
-        if (cell == 1) {
-            board[0][0] = player.marker;
-        } 
-        else if (cell == 2) {
-            board[0][1] = player.marker;
-        } 
-        else if (cell == 3) {
-            board[0][2] = player.marker;
-        } 
-        else if (cell == 4) {
-            board[1][0] = player.marker;
-        } 
-        else if (cell == 5) {
-            board[1][1] = player.marker;
-        } 
-        else if (cell == 6) {
-            board[1][2] = player.marker;
-        } 
-        else if (cell == 7) {
-            board[2][0] = player.marker;
-        } 
-        else if (cell == 8) {
-            board[2][1] = player.marker;
-        } 
-        else if (cell == 9) {
-            board[2][2] = player.marker;
+        if (gameOver === true) return;
+        const row = Math.floor((cell - 1) / 3);
+        const col = (cell - 1) % 3;
+        board[row][col] = player.marker;
+    }
+
+    const clearBoard = () => {
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < columns; j++) {
+                board[i][j] = empty;
+            } 
         }
     }
 
     const printBoard = () => {
+        if (gameOver === true) return;
         let boardConsole = "";
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < columns; j++) {
@@ -68,29 +78,122 @@ function GameBoard() {
         }
         console.log(boardConsole);
     }
+    return {getBoard, placeMarker, printBoard, clearBoard};
+})();
 
-    return {getBoard, placeMarker, printBoard};
+
+function startGame(p1Name, p2TwoName) {
+    p1.textContent = p1Name;
+    p2.textContent = p2TwoName;
 }
 
-function GameController() {
-    const playerOneName = "e";
-    const playerTwoName = "a";
 
-    //tästä oma aliohjelmansa joka tekee tämän
-    p1.textContent = playerOneName;
-    p2.textContent = playerTwoName;
-
-    const playerOneMarker = "e";
-    const playerTwoMarker = "a";
-
-    const players = [
-        {
-            name: playerOneName,
-            marker: playerOneMarker
-        },
-        {
-            name: playerTwoName,
-            marker: playerTwoMarker 
+function placeMarkerUI(cell, player) {
+    if (gameOver === true) return;
+    for (const cell1 of cells) {
+        if (cell1.id == cell) {
+            cell1.textContent = player.marker;
         }
-    ];
+    }
+}
+
+
+function isGameOver() {
+    let lauta = game.getBoard()
+    //horizontal check
+    for (row of lauta) {
+        if (row[0] === row[1] && row[1] === row[2] && row[0] != "") return 1;
+    }
+
+    //vertical check
+    for (let i = 0; i < 3; i++) {
+        if (lauta[0][i] === lauta[1][i] && lauta[1][i] === lauta[2][i] && lauta[0][i] != "") return 1;
+    }
+
+    //crisscross check
+    if (lauta[0][0] === lauta[1][1] && lauta[1][1] === lauta[2][2] && lauta[0][0] != "") return 1;
+    if (lauta[0][2] === lauta[1][1] && lauta[1][1] === lauta[2][0] && lauta[0][2] != "") return 1;
+
+    //tie check
+    let notEmptyCells = 0;
+    for (row of lauta) {
+        for (cell of row) {
+            if (cell != empty) notEmptyCells += 1;
+        }
+        if (notEmptyCells === 9) return 2;
+    }
+    return 0;
+}
+
+
+function checkIsGameOver(pelaaja) {
+    if (gameOver === true) return;
+    if (isGameOver() === 1) {
+        result.textContent = `Game over, ${pelaaja.name} won the game!`;
+        gameOver = true;
+        again.style.display = 'block';
+        return;
+    }
+    else if (isGameOver() === 2) {
+        result.textContent = "Its a tie!";
+        again.style.display = 'block';
+        gameOver = true;
+        return;
+    }
+}
+
+
+function switchPelaaja(pelaaja, players) {
+    if (pelaaja === players[0]) {
+        pelaaja = players[1];
+    } else {
+        pelaaja = players[0];
+    }
+}
+
+
+function clearUi() {
+    for (const cell of cells) {
+        cell.textContent = empty;
+    }
+}
+
+
+function newGame() {
+    game.clearBoard();
+    clearUi();
+    gameOver = false;
+    result.textContent = "";
+    GameController();
+    
+}
+
+
+function GameController() {
+    let pelaaja = players[0];
+    turn.textContent = `It's ${pelaaja.name}'s turn`;
+
+    function switchPelaaja() {
+        if (pelaaja === players[0]) {
+            pelaaja = players[1];
+        } else {
+            pelaaja = players[0];
+        }
+    }
+
+    startGame(playerOneName, playerTwoName);
+
+    document.querySelectorAll(".cell").forEach(cell => {
+        cell.addEventListener("click", function handleClick(event) {
+            const cellNum = event.target.id;
+
+            game.placeMarker(cellNum, pelaaja);
+            game.printBoard();
+            placeMarkerUI(cellNum, pelaaja);
+
+            checkIsGameOver(pelaaja); 
+
+            switchPelaaja();
+        });
+    });
 }
