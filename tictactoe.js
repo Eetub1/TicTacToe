c7dd7602358a80b40d26c70cf8878fca2d53a720
Eetu <eetu.oinonen04@gmail.com>
2025-04-18 19:18:58 +0300

    Game logic functions now properly. Next improve UI

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,14 @@
-let players = []
-//muutostesti läppärillä
+let players = [
+    {
+        name: "",
+        marker: ""
+    },
+    {
+        name: "",
+        marker: "" 
+    }
+];
+
 
 const start = document.getElementById("start");
 start.addEventListener("click", () => GameController());
@@ -7,29 +16,6 @@ start.addEventListener("click", () => GameController());
 const closeBtn = document.getElementById("close");
 closeBtn.addEventListener("click", () => popup.close());
 const form = document.getElementById("form");
-
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const playerOneName = document.getElementById("player1name").value;
-    const playerTwoName = document.getElementById("player2name").value;
-    const playerOneMarker = document.getElementById("player1marker").value;
-    const playerTwoMarker = document.getElementById("player2marker").value;
-
-    players = [
-        {
-            name: playerOneName,
-            marker: playerOneMarker
-        },
-        {
-            name: playerTwoName,
-            marker: playerTwoMarker 
-        }
-    ];
-    
-    form.reset();
-    popup.close();
-})
 
 const again = document.getElementById("again");
 again.style.display = 'none';
@@ -42,6 +28,26 @@ const p2 = document.getElementById("player2");
 const turn = document.getElementById("turn");
 const result = document.getElementById("result");
 const cells = document.querySelectorAll(".cell");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const playerOneName = document.getElementById("player1name").value;
+    const playerTwoName = document.getElementById("player2name").value;
+    const playerOneMarker = document.getElementById("player1marker").value;
+    const playerTwoMarker = document.getElementById("player2marker").value;
+
+    players[0].name = playerOneName;
+    players[0].marker = playerOneMarker;
+    players[1].name = playerTwoName;
+    players[1].marker = playerTwoMarker;
+
+    p1.textContent = players[0].name;
+    p2.textContent = players[1].name;
+    
+    form.reset();
+    popup.close();
+})
 
 const empty = "";
 let gameOver = false;
@@ -146,14 +152,15 @@ function checkIsGameOver(pelaaja) {
         result.textContent = `Game over, ${pelaaja.name} won the game!`;
         gameOver = true;
         again.style.display = 'block';
-        return;
+        return 1;
     }
     else if (isGameOver() === 2) {
         result.textContent = "Its a tie!";
         again.style.display = 'block';
         gameOver = true;
-        return;
+        return 1;
     }
+    return 0;
 }
 
 
@@ -184,7 +191,7 @@ function switchPelaaja(pelaaja) {
 
 
 function GameController() {
-    if (!players.length) popup.show();
+    if (players[0].name === "") popup.show();
     let pelaaja = players[0];
     turn.textContent = `It's ${pelaaja.name}'s turn`;
 
@@ -198,7 +205,8 @@ function GameController() {
             game.printBoard();
             placeMarkerUI(cellNum, pelaaja);
 
-            checkIsGameOver(pelaaja); 
+            const num = checkIsGameOver(pelaaja); 
+            if (num === 1) return;
 
             pelaaja = switchPelaaja(pelaaja);
             turn.textContent = `It's ${pelaaja.name}'s turn`;
