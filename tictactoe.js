8a9a9501f2986a87635e527bb19348a0c2f040a3
eeeeoino <eetu.e.oinonen@student.jyu.fi>
2025-06-23 19:06:45 +0300

    drastically improved js and fixed all bugs

--- tictactoe.js
+++ tictactoe.js
@@ -1,115 +1,105 @@
-let players = []
-
-const start = document.getElementById("start");
-start.addEventListener("click", () => GameController());
-
-const closeBtn = document.getElementById("close");
-closeBtn.addEventListener("click", () => popup.close());
-const form = document.getElementById("form");
-
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const playerOneName = document.getElementById("player1name").value;
-    const playerTwoName = document.getElementById("player2name").value;
-    const playerOneMarker = document.getElementById("player1marker").value;
-    const playerTwoMarker = document.getElementById("player2marker").value;
-
-    players = [
-        {
-            name: playerOneName,
-            marker: playerOneMarker
-        },
-        {
-            name: playerTwoName,
-            marker: playerTwoMarker 
-        }
-    ];
-    
-    form.reset();
-    popup.close();
-})
-
-const again = document.getElementById("again");
-again.style.display = 'none';
-again.addEventListener("click", newGame);
-
-const popup = document.querySelector("dialog");
-
-const p1 = document.getElementById("player1");
-const p2 = document.getElementById("player2");
-const turn = document.getElementById("turn");
-const result = document.getElementById("result");
-const cells = document.querySelectorAll(".cell");
-
-const empty = "";
-let gameOver = false;
-
-const game = (function GameBoard() {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
-
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i][j] = empty;
-        } 
+const players = [
+    {
+        name: "p1",
+        marker: "X"
+    },
+    {
+        name: "p2",
+        marker: "O"
     }
+]
 
-    const getBoard = () => board;
+//object that has the player's info whose turn it is
+let whoseTurn
+//what represents an empty cell on the board
+const empty = ""
+//if false game is not over, if true game is over
+let gameState = false
 
-    const placeMarker = (cell, player) => {
-        if (gameOver === true) return;
-        const row = Math.floor((cell - 1) / 3);
-        const col = (cell - 1) % 3;
-        board[row][col] = player.marker;
-    }
+const startBtn = document.getElementById("start")
+startBtn.addEventListener("click", startGame)
 
-    const clearBoard = () => {
-        for (let i = 0; i < rows; i++) {
-            board[i] = [];
-            for (let j = 0; j < columns; j++) {
-                board[i][j] = empty;
-            } 
-        }
-    }
-
-    const printBoard = () => {
-        if (gameOver === true) return;
-        let boardConsole = "";
-        for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < columns; j++) {
-                if (j != 2) {
-                    boardConsole += board[i][j] + "|";
-                }
-                else {
-                    boardConsole += board[i][j];
-                } 
-            }
-            boardConsole += "\n";
-        }
-        console.log(boardConsole);
-    }
-    return {getBoard, placeMarker, printBoard, clearBoard};
-})();
+const player1name = document.getElementById("player1")
+const player2name = document.getElementById("player2")
+const turn = document.getElementById("turn")
+const cells = document.querySelectorAll(".cell")
+const info = document.getElementById("info")
+const result = document.getElementById("result")
+const again = document.getElementById("again")
+again.addEventListener("click", startNewGame)
+again.style.display = "none"
 
 
 function startGame() {
-    p1.textContent = players[0].name;
-    p2.textContent = players[1].name;
+    player1name.textContent = players[0].name
+    player2name.textContent = players[1].name
+    turn.textContent = players[0].name
+    whoseTurn = players[0]
+
+    cells.forEach(cell => {
+        cell.addEventListener("click", (event) => handleClick(event))
+    })
+
+    startBtn.style.display = "none"
 }
 
+function handleClick(event) {
+    if (gameState === true) return
 
-function placeMarkerUI(cell, player) {
-    if (gameOver === true) return;
-    for (const cell1 of cells) {
-        if (cell1.id == cell) {
-            cell1.textContent = player.marker;
-        }
+    const wasSuccess = game.placeMarker(event.target.id, whoseTurn)
+    if (!wasSuccess) {
+        info.textContent = "That cell is already taken!"
+        return
+    }
+    info.textContent = ""
+    game.printBoard()
+    placeMarkerUI(event)
+    const isOver = checkIsGameOver()
+    if (isOver) {
+        again.style.display = "block"
+        return
+    }
+    switchTurn(whoseTurn)
+    turn.textContent = whoseTurn.name
+}
+
+function startNewGame() {
+    result.textContent = ""
+    turn.textContent = ""
+    clearUi()
+    game.clearBoard()
+    whoseTurn = players[0]
+    turn.textContent = whoseTurn.name
+    gameState = false
+}
+
+function clearUi() {
+    for (const cell of cells) {
+        cell.textContent = empty;
     }
 }
 
+function placeMarkerUI(event) {
+    event.target.textContent = whoseTurn.marker
+}
+
+function switchTurn(player) {
+    whoseTurn = (player === players[0]) ? players[1] : players[0]
+}
+
+function checkIsGameOver() {
+    if (isGameOver() === 1) {
+        result.textContent = `Game over, ${whoseTurn.name} won the game!`;
+        gameState = true
+        return 1;
+    }
+    else if (isGameOver() === 2) {
+        result.textContent = "Its a tie!";
+        gameState = true
+        return 1;
+    }
+    return 0
+}
 
 function isGameOver() {
     let lauta = game.getBoard()
@@ -138,69 +128,54 @@ function isGameOver() {
     return 0;
 }
 
+const game = (function GameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
 
-function checkIsGameOver(pelaaja) {
-    if (gameOver === true) return;
-    if (isGameOver() === 1) {
-        result.textContent = `Game over, ${pelaaja.name} won the game!`;
-        gameOver = true;
-        again.style.display = 'block';
-        return;
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i][j] = empty;
+        } 
     }
-    else if (isGameOver() === 2) {
-        result.textContent = "Its a tie!";
-        again.style.display = 'block';
-        gameOver = true;
-        return;
+
+    const getBoard = () => board;
+
+    const placeMarker = (cell, player) => {
+        const row = Math.floor((cell - 1) / 3);
+        const col = (cell - 1) % 3;
+        if (board[row][col] !== empty) {
+            return false
+        }
+        board[row][col] = player.marker;
+        return true
     }
-}
 
-
-function clearUi() {
-    for (const cell of cells) {
-        cell.textContent = empty;
+    const clearBoard = () => {
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < columns; j++) {
+                board[i][j] = empty;
+            } 
+        }
     }
-}
 
-
-function newGame() {
-    game.clearBoard();
-    clearUi();
-    gameOver = false;
-    result.textContent = "";
-    GameController();
-}
-
-
-function switchPelaaja(pelaaja) {
-    if (pelaaja === players[0]) {
-        pelaaja = players[1];
-    } else {
-        pelaaja = players[0];
+    const printBoard = () => {
+        let boardConsole = "";
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                if (j != 2) {
+                    boardConsole += board[i][j] + "|";
+                }
+                else {
+                    boardConsole += board[i][j];
+                } 
+            }
+            boardConsole += "\n";
+        }
+        console.log(boardConsole);
     }
-    return pelaaja
-}
+    return {getBoard, placeMarker, printBoard, clearBoard};
+})();
 
-
-function GameController() {
-    if (!players.length) popup.show();
-    let pelaaja = players[0];
-    turn.textContent = `It's ${pelaaja.name}'s turn`;
-
-    startGame();
-
-    document.querySelectorAll(".cell").forEach(cell => {
-        cell.addEventListener("click", function handleClick(event) {
-            const cellNum = event.target.id;
-
-            game.placeMarker(cellNum, pelaaja);
-            game.printBoard();
-            placeMarkerUI(cellNum, pelaaja);
-
-            checkIsGameOver(pelaaja); 
-
-            pelaaja = switchPelaaja(pelaaja);
-            turn.textContent = `It's ${pelaaja.name}'s turn`;
-        });
-    });
-}
